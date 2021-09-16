5fbfa325ec2f1f5fb612e5648c010ed3167d8049
artsiomivanets <artem.ivanec96@gmail.com>
2021-09-16 11:42:16 +0300

    move prettier to root

--- react-app/src/pages/home-page/model/index.ts
+++ react-app/src/pages/home-page/model/index.ts
@@ -1,10 +1,10 @@
-import { createDomain, sample } from "effector";
-const domain = createDomain("home-page");
+import { createDomain, sample } from 'effector';
+const domain = createDomain('home-page');
 
 const downloadDataFx = domain.createEffect(async () => {
-  const res = await fetch("http://localhost:8888/testApi", {
+  const res = await fetch('http://localhost:8888/testApi', {
     headers: {
-      Authorization: "Basic cm9vdDpzZWNyZXQ=",
+      Authorization: 'Basic cm9vdDpzZWNyZXQ=',
     },
   });
   return res.json();
@@ -13,7 +13,7 @@ const downloadDataFx = domain.createEffect(async () => {
 const $patient = domain.createStore({});
 
 $patient.on(downloadDataFx.doneData, (_, payload) => payload);
-$patient.watch((store) => console.log("PATIENT STORE ->", store));
+$patient.watch((store) => console.log('PATIENT STORE ->', store));
 
 export const downloadData = domain.createEvent();
 
