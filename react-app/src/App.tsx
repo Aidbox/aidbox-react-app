c98907db508d25bd1c377d5afa03f49057fd5d66
artsiomivanets <artem.ivanec96@gmail.com>
2021-09-17 16:05:49 +0300

    routing

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,7 +1,58 @@
 import { HomePage } from './pages/home-page';
+import { useRoutes, BrowserRouter, Outlet } from 'react-router-dom';
 
-const App = () => {
-  return <HomePage />;
+const Apps = () => {
+  return <div>Apps</div>;
 };
 
-export default App;
+const AppPage = () => {
+  return (
+    <div>
+      <div> AppPage</div>
+      <Outlet />
+    </div>
+  );
+};
+
+const AppPageKey = () => {
+  return <div>AppPage key</div>;
+};
+
+const Layout = () => {
+  return (
+    <div>
+      <ul>
+        <li>asdfsadf</li>
+        <li>asdfsadf</li>
+        <li>asdfsadf</li>
+      </ul>
+      <div style={{ margin: '20' }}>
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+// Outlet as yeild
+
+const App = () => {
+  const main = useRoutes([
+    { path: '/', element: <HomePage /> },
+    {
+      path: '/apps',
+      element: <Layout />,
+      children: [
+        { element: <Apps /> },
+        { path: ':id', element: <AppPage />, children: [{ path: ':key', element: <AppPageKey /> }] },
+      ],
+    },
+  ]);
+
+  return main;
+};
+
+export default () => (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
